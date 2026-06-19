9fd803f01fb96e1ed529446834d21a02eb05fc69
romnn <roman.dahm@gmx.de>
2026-06-19 05:33:15 +0200

    examples: add go and python polyglot projects
    Exercise the go and uv adapters end to end from in-repo example
    projects.

---
+++ examples/polyglot/go/main.go
@@ -0,0 +1,11 @@
+// Example module for cooldown screenshots; pins old dependency versions on purpose.
+package main
+
+import (
+	_ "github.com/google/uuid"
+	_ "github.com/sirupsen/logrus"
+	_ "github.com/spf13/cobra"
+	_ "github.com/stretchr/testify/assert"
+)
+
+func main() {}
